5b4e5497496f4281ec1677b7bce471f4c74df259
Pr1thu1 <25566@students.mrgs.school.nz>
2026-05-14 13:23:23 +1200

    adding....

--- Assessment.py
+++ Assessment.py
@@ -1,32 +1,47 @@
-# Importing Tkinter and using PIL
+# Imported tkinter and PIL
 import tkinter as tk
 from PIL import Image, ImageTk
 
-# Defining the size of the image
 root = tk.Tk()
-root.geometry("1920x1080")
+# Sizing to fit different screens
+width = root.winfo_screenwidth()
+height = root.winfo_screenheight()
+root.geometry(f"{width}x{height}")
 
-# Opening the image
+# Background image
 image = Image.open("background2.png")
-# --- ADDED THE RESIZE LINE BELOW ---
-image = image.resize((1920, 1080), Image.LANCZOS)
+image = image.resize((width, height), Image.LANCZOS)
 bg_image = ImageTk.PhotoImage(image)
 
-# Making the image the background image
 bg_label = tk.Label(root, image=bg_image)
-
-# Using .place to cover the entire screen
 bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 bg_label.image = bg_image
 
-# Entry box
-name_entry = tk.Entry(root, font=("Arial", 20), justify="center")
+# Asking user to enter their name
+title_label = tk.Label(root, text="Please Enter Your Name:", font=("Arial", 20, "bold"),
+                       fg="#1a5276", bg="#ffffff")
+title_label.place(relx=0.5, rely=0.76, anchor="center")
 
-# Puts in the middle of the screen
-name_entry.place(relx=0.5, rely=0.45, anchor="center")
+# Name Entry Box
+name_entry = tk.Entry(root, font=("Arial", 24), justify="center",
+                      bg="#C8EDF7", fg="black", bd=0, relief="sunken",
+                      highlightbackground="#ffffff", highlightthickness=2)
+name_entry.place(relx=0.5, rely=0.83, width=400, height=50, anchor="center")
 
 # Continue button
-continue_btn = tk.Button(root, text="Continue", font=("Arial", 15), width=15)
-continue_btn.place(relx=0.5, rely=0.52, anchor="center")
+continue_btn = tk.Label(
+    root,
+    text="▶",
+    font=("Arial", 45, "bold"),
+    width=2,
+    bg="#c8edf7",
+    fg="white",
+    cursor="hand2",
+    bd=0,
+    relief="flat"
+)
 
+continue_btn.place(relx=0.5, rely=0.93, anchor="center")
+
+# Loop program
 root.mainloop()
